e34aa44c2d1f67193e697d9a93fbcdea3e8ca266
Samuel Sieberg <samuel.sieberg@stud.uni-due.de>
2020-01-01 17:03:54 +0100

    Libgdx project init.

---
+++ desktop/src/com/verminsnest/gamedev/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.verminsnest.gamedev.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.verminsnest.gamedev.Vermins-Nest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Vermins-Nest(), config);
+	}
+}
